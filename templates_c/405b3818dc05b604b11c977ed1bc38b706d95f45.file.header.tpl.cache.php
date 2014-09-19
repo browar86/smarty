84b1e7223a714822f01ac6cd97ceaa994590c71c
Krzychu Bodusz <k-bodusz@wp.pl>
2014-09-19 12:34:58 +0200

    Init

---
+++ templates_c/405b3818dc05b604b11c977ed1bc38b706d95f45.file.header.tpl.cache.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2014-09-19 10:32:14
+         compiled from ".\templates\header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:27553541c062e4a6980-93377171%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '405b3818dc05b604b11c977ed1bc38b706d95f45' => 
+    array (
+      0 => '.\\templates\\header.tpl',
+      1 => 1411122625,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '27553541c062e4a6980-93377171',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+    'Name' => 1,
+  ),
+  'has_nocache_code' => true,
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_541c062e4b15f3_91798502',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_541c062e4b15f3_91798502')) {function content_541c062e4b15f3_91798502($_smarty_tpl) {?><HTML>
+<HEAD>
+    <TITLE><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+ - <?php echo '/*%%SmartyNocache:27553541c062e4a6980-93377171%%*/<?php echo $_smarty_tpl->tpl_vars[\'Name\']->value;?>
+/*/%%SmartyNocache:27553541c062e4a6980-93377171%%*/';?>
+</TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
+<?php }} ?>
